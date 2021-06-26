957f1b19768881cba5f88411e17c6879bd35ee8b
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-26 20:47:23 +0000

    solve exercise 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ from datetime import datetime
 class BaseModel:
     """ define class base"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialize public instance attributes """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """ return string representation of basemodel"""
