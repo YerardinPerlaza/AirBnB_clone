e126375a9c23b32903666b5d5da8da3afe269c58
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-29 20:09:54 +0000

    edge case base model

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Initialize public instance attributes """
+        if (kwargs == {}):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
-            if kwargs:
-                kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                         "%Y-%m-%dT%H:%M:%S.%f")
-                kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                         "%Y-%m-%dT%H:%M:%S.%f")
-                for key, val in kwargs.items():
-                    if "__class__" not in key:
-                        setattr(self, key, val)
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
         """ return string representation of basemodel"""
