59f8f9b89111f9026377ba0ebb0a38a296acfbe3
Santiago Cano <2704@holbertonschool.com>
2021-06-28 20:48:09 -0500

    add task 9

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+''' Amenity Class '''
+
+
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+    Inherits Public class attributes from BaseModel:
+
+    name: <str>
+    '''
+    name = ""
