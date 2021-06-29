1d29513f7b95ced829a54940e687942f2c93312e
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-29 20:46:02 +0000

    another try of task 3

--- models/base_model.py
+++ models/base_model.py
@@ -11,14 +11,16 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ Initialize public instance attributes """
         if kwargs:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
