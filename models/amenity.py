b191be18f3ed11fe678fd715262d519a0f441cc9
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-29 15:39:19 +0000

    check long comments

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,10 @@
 #!usr/bin/python3
 ''' Amenity Class '''
-
-
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     '''
-    Inherits Public class attributes from BaseModel:
-
     name: <str>
     '''
     name = ""
