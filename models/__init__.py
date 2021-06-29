085428919ee8dcd6861d16991dd5060ed3530a19
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-29 15:46:46 +0000

    checker imp

--- models/__init__.py
+++ models/__init__.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """Initializer"""
 from models.engine.file_storage import FileStorage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-import models
 
 storage = FileStorage()
 storage.reload()
